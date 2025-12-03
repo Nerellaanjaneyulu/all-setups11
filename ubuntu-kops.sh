20d9f5dd2a979b9f89de2160037dd2b79dfddec6
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-12-03 11:14:14 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -19,3 +19,4 @@ aws s3api create-bucket --bucket anjaneyulubucket002.k8s.local --region us-east-
 aws s3api put-bucket-versioning --bucket anjaneyulubucket002.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://anjaneyulubucket002.k8s.local
 kops create cluster --name anjaneyulu.k8s.local --zones us-east-1a --control-plane-image ami-0fa3fe0fa7920f68e --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-0fa3fe0fa7920f68e --node-count=2 --node-size c7i-flex.large
+kops update cluster --name anjaneyulu.k8s.local --yes --admin
