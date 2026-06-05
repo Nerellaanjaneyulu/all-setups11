824d68d6a59884d3e074717f732853d4fb8e9d01
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2026-06-05 10:09:51 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -15,15 +15,15 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket ramakrishnabucket2628.k8s.local  --region ap-south-1
-aws s3api put-bucket-versioning --bucket ramakrishnabucket2628.k8s.local  --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://ramakrishnabucket2628.k8s.local 
-kops create cluster --name anjaneyulu.k8s.local --zones ap-south-1 --control-plane-image ami-024ebedf48d280810 --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-024ebedf48d280810 --node-count=2 --node-size c7i-flex.large
+aws s3api create-bucket --bucket anjaneyulubucket111.k8s.local  --region us-east-1
+aws s3api put-bucket-versioning --bucket anjaneyulubucket111.k8s.local  --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3:// anjaneyulubucket111.k8s.local
+kops create cluster --name anjaneyulu.k8s.local --zones us-east-1 --control-plane-image ami-091138d0f0d41ff90 --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-091138d0f0d41ff90 --node-count=2 --node-size c7i-flex.large
 kops update cluster --name anjaneyulu.k8s.local --yes --admin
 
 
 
-export KOPS_STATE_STORE=s3://ramakrishnabucket2628.k8s.local 
+export KOPS_STATE_STORE=s3:// anjaneyulubucket111.k8s.local
 echo $KOPS_STATE_STORE
 
 
